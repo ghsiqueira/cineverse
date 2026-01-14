fa3679941af202557d959fff3957ce3a949a4f11
ghsiqueira <gabrielh2805@gmail.com>
2026-01-14 19:21:19 -0300

    chore: setup global styles with custom color palette and axios config

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  params: {
+    api_key: import.meta.env.VITE_API_KEY,
+    language: 'en-US', 
+  },
+});
+
+export default api;
